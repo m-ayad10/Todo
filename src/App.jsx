c55f3e963e36b8fe88ecfc76606b58c5027be2ac
m-ayad10 <ayadmohammed403@gmail.com>
2025-08-26 11:59:34 +0530

    Edit added

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@ import "./App.css";
 function App() {
   const [input, setInput] = useState("");
   const [todo, setTodo] = useState([]);
+  const [edit, setEdit] = useState(-1);
+  const [editValue, setEditValue] = useState("");
   const handleTodo = () => {
     setTodo((prev) => {
       return [...prev, input];
     });
-    setInput('')
+    setInput("");
   };
-  const handleDelete=(index)=>{
-    setTodo((prev)=>{
-      return prev.toSpliced(index,1)
-    })
-  }
+  const handleDelete = (index) => {
+    setTodo((prev) => {
+      return prev.toSpliced(index, 1);
+    });
+  };
+  const handleEdit = () => {
+    setTodo((prev) => {
+      return prev.with(edit, editValue);
+    });
+    setEdit(-1);
+    setEditValue("");
+  };
+
   return (
     <>
       <h1>Todo</h1>
@@ -28,16 +38,36 @@ function App() {
         />
         <button onClick={handleTodo}>Add</button>
         <br />
-        {
-          todo.map((value,index)=>{
-            return(
-              <div key={index}>
-                <input type="text" value={value} disabled />
-                <button onClick={()=>handleDelete(index)}>Delete</button>
-              </div>
-            )
-          })
-        }
+        {todo.map((value, index) => {
+          return (
+            <div key={index}>
+              {edit === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={editValue}
+                    onChange={(e) => setEditValue(e.target.value)}
+                  />
+                  <button onClick={handleEdit}>Done</button>
+                </>
+              ) : (
+                <>
+                  <input type="text" value={value} disabled />
+                  <button
+                    onClick={() => {
+                      console.log(index);
+                      setEditValue(value);
+                      setEdit(index);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </>
+              )}
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          );
+        })}
       </div>
     </>
   );
